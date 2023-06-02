6b305df7538ee214ab9609206c69b8a4595ff937
Ivan <frexozagsap@yandex.ru>
2023-06-02 21:34:11 +0300

    Delete history-site (two) directory

--- history-site (two)/app.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
-const polk86 = document.getElementById("polk86");
-
-burger.addEventListener('click', () => {
-    menu.classList.toggle('disp');
-});
-
-polk86.scrollIntoView({ block: "end" });
